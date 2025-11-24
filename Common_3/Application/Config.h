87bd6aa92d58c0fb370afc50fde552fd96e4ca8e
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2025-11-23 23:05:49 -0500

    Remove Visual Studio version whitelist check

--- Common_3/Application/Config.h
+++ Common_3/Application/Config.h
@@ -238,13 +238,6 @@ COMPILE_ASSERT(sizeof(ssize_t) == sizeof(int64_t));
 #define WINVER        _WIN32_WINNT_WIN7
 #define _WIN32_WINNT  _WIN32_WINNT_WIN7
 
-// Whitelist of compiler versions
-#if (_MSC_VER == 1929) // VS 2019 all VC++ compilers
-#else
-#pragma message("Bad Visual Studio version: (" QUOTE(_MSC_VER) " " QUOTE(_MSC_FULL_VER) " " QUOTE(_MSC_BUILD) ").")
-#error "Bad Visual Studio version"
-#endif
-
 #endif
 
 #elif defined(__APPLE__)
